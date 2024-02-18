6c6280f1e2b0265f0c0fce417fd3ff486ef64fd5
Lillian Torres <70078750+lillibun@users.noreply.github.com>
2024-02-18 00:24:59 -0600

    adding placeholder images + slideshow code

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-document.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
-    var background = document.querySelector('.header-bg');
+// document.addEventListener('scroll', function() {
+//     var scrollPosition = window.scrollY;
+//     var background = document.querySelector('.header-bg');
 
-  });
-  
+//   });
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
